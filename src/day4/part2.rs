c435ebe1e652cea690114fb1e64fe6b0195f2b6e
Riste Mingov <riste.mingov@gmail.com>
2023-12-06 00:33:31 +0100

    wip

--- src/day4/part2.rs
+++ src/day4/part2.rs
@@ -1,15 +1,10 @@
-
 use crate::utils;
 
-
 pub fn solve(file_path: &str) {
-
     println!("In file {}", file_path);
-    
-    if let Ok(lines) = utils::misc::read_lines(file_path) {
 
+    if let Ok(lines) = utils::misc::read_lines(file_path) {
         for (i, line) in lines.enumerate() {
-            
         }
     }
 }
