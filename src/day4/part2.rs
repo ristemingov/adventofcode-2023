0f31c6fe5b9543db7b20fb3e01a0e361dc8fc69b
ristemingov <riste.mingov@gmail.com>
2023-12-09 22:52:28 +0100

    part2

--- src/day4/part2.rs
+++ src/day4/part2.rs
@@ -1,10 +1,12 @@
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 use crate::utils;
 
 pub fn solve(file_path: &str) {
     println!("In file {}", file_path);
     let mut _sum: i32 = 0;
+    let mut count_vec: Vec<i32> = Vec::new();
     if let Ok(lines) = utils::misc::read_lines(file_path) {
         for (_i, _line) in lines.enumerate() {
             if let Ok(row) = _line {
@@ -24,9 +26,39 @@ pub fn solve(file_path: &str) {
                         .filter(|num| str::parse::<i32>(num).is_ok())
                         .cloned(),
                 );
-                let intersection = winning_set.intersection(&my_selection_set);
+                let intersection: HashSet<_> = winning_set
+                    .intersection(&my_selection_set)
+                    .cloned()
+                    .collect();
+                let mut _get: i32 = 0;
+
+                match count_vec.get(_i) {
+                    Some(x) => {
+                        _get = *x;
+                    }
+                    None => {
+                        _get = 1;
+                        count_vec.push(1);
+                    }
+                }
+
+                for _m in 0.._get {
+                    for k in 1..=intersection.len() {
+                        match count_vec.get(_i + k) {
+                            Some(_x) => {
+                                count_vec[_i + k] += 1;
+                            }
+                            None => {
+                                count_vec.push(2);
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+    for i in count_vec {
+        _sum += i;
     }
     println!("{}", _sum);
 }
