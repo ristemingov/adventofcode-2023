49bac4fab8e83d36566ac655e8e7f5c15749b3fe
ristemingov <riste.mingov@gmail.com>
2023-12-10 21:28:41 +0100

    Day 6: Day 6: Wait For It

--- src/day6/part1.rs
+++ src/day6/part1.rs
@@ -3,10 +3,40 @@ use crate::utils;
 pub fn solve(file_path: &str) {
     println!("In file {}", file_path);
 
+    let mut time = Vec::new();
+    let mut distance = Vec::new();
+
     if let Ok(lines) = utils::misc::read_lines(file_path) {
         for (_i, _line) in lines.enumerate() {
-            if let Ok(_row) = _line {}
+            if let Ok(row) = _line {
+                if row.starts_with("Time:") {
+                    let numbers: Vec<u64> = row
+                        .split_whitespace()
+                        .filter_map(|s| s.parse().ok())
+                        .collect();
+                    time.extend(numbers);
+                } else if row.starts_with("Distance:") {
+                    let numbers: Vec<u64> = row
+                        .split_whitespace()
+                        .filter_map(|s| s.parse().ok())
+                        .collect();
+                    distance.extend(numbers);
+                }
+            }
         }
     }
-    println!("Not implemented yet");
+    let mut product = 1;
+    for i in 0..time.len() {
+        let mut sum = 0;
+        for j in 0..time[i] {
+            if j * (time[i] - j) > distance[i] {
+                sum += 1;
+            }
+        }
+
+        if sum > 0 {
+            product *= sum;
+        }
+    }
+    println!("Product: {}", product);
 }
--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -3,10 +3,44 @@ use crate::utils;
 pub fn solve(file_path: &str) {
     println!("In file {}", file_path);
 
+    let mut time_num: u64 = 0;
+    let mut distance_num: u64 = 0;
+
     if let Ok(lines) = utils::misc::read_lines(file_path) {
         for (_i, _line) in lines.enumerate() {
-            if let Ok(_row) = _line {}
+            if let Ok(row) = _line {
+                if row.starts_with("Time:") {
+                    let numbers: Vec<u64> = row
+                        .split_whitespace()
+                        .filter_map(|s| s.parse().ok())
+                        .collect();
+                    time_num = numbers
+                        .into_iter()
+                        .map(|i| i.to_string())
+                        .collect::<String>()
+                        .parse::<u64>()
+                        .unwrap();
+                } else if row.starts_with("Distance:") {
+                    let numbers: Vec<u64> = row
+                        .split_whitespace()
+                        .filter_map(|s| s.parse().ok())
+                        .collect();
+                    distance_num = numbers
+                        .into_iter()
+                        .map(|i| i.to_string())
+                        .collect::<String>()
+                        .parse::<u64>()
+                        .unwrap();
+                }
+            }
         }
     }
-    println!("Not implemented yet");
+
+    let mut sum = 0;
+    for j in 0..time_num {
+        if j * (time_num - j) > distance_num {
+            sum += 1;
+        }
+    }
+    println!("Sum: {}", sum)
 }
